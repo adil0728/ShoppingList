3633307f5522a05ec0aeb5dba731ed5af62c31e1
Adil lakkhlifi <lakxlifi@mail.ru>
2023-07-10 12:46:14 +0300

    Add room

--- app/src/main/java/com/example/shoppinglist/data/ShopItemDbModel.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopItemDbModel.kt
@@ -1,14 +1,13 @@
 package com.example.shoppinglist.data
 
-data class ShopItem(
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+@Entity(tableName = "shop_items")
+data class ShopItemDbModel(
+@PrimaryKey(autoGenerate = true)
+    val id: Int,
     val name: String,
     val count: Int,
-    val enabled: Boolean,
-    var id: Int = UNDEFINED_ID
-) {
-
-    companion object{
-        const val  UNDEFINED_ID = -1
-    }
-}
+    val enabled: Boolean
+)
